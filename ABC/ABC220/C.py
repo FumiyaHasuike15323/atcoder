d4bed99b824c238330c52d9fdabb86b3e466e40d
FumiyaHasuike15323 <humiyahasuike@gmail.com>
2021-12-28 14:03:14 +0900

    ABC220 C.py

--- ABC/ABC220/C.py
+++ ABC/ABC220/C.py
@@ -1,12 +1,12 @@
-N = int(input())
-a = list(map(int,input().split()))
-x = int(input())
-cnt = x // (sum(a))
-rest = x % (sum(a))
-ans = cnt * N
-for i in range(N):
-    if rest - a[i] < 0:
-        ans += i+1
-        break
-    rest -= a[i]
-print(ans)
+# N = int(input())
+# a = list(map(int,input().split()))
+# x = int(input())
+# cnt = x // (sum(a))
+# rest = x % (sum(a))
+# ans = cnt * N
+# for i in range(N):
+#     if rest - a[i] < 0:
+#         ans += i+1
+#         break
+#     rest -= a[i]
+# print(ans)
